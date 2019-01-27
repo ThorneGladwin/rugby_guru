85d92f6efad6b44198212698f30977451fe9cf0a
Unknown <thorne.gladwin@gmail.com>
2019-01-27 23:09:29 +0000

    scraper logic

---
+++ constants.js
@@ -0,0 +1,34 @@
+const LIST_OF_TEAMS = [
+"Bath",
+"Bristol Bears",
+"Exeter Chiefs",
+"Gloucester",
+"Harlequins",
+"Leicester Tigers",
+"Newcastle Falcons",
+"Northampton Saints",
+"Sale Sharks",
+"Saracens",
+"Wasps",
+"Worcester Warriors"
+];
+
+const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+module.exports = {
+    LIST_OF_TEAMS,
+    MONTHS
+};
