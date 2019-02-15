4664ea7f7690ee73793c4759599b2df80327fcd1
Unknown <thorne.gladwin@gmail.com>
2019-02-15 21:40:49 +0000

    Improved Introduction speech responses

--- lambda/custom/handlers/coreHandlers.js
+++ lambda/custom/handlers/coreHandlers.js
@@ -1,14 +1,83 @@
+const get = require("lodash/get");
 const { CoreIntentsSpeech } = require("../resources/coreIntents");
+const { ErrorsSpeech } = require("../handlers/errorHandler");
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === "LaunchRequest";
   },
   handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(CoreIntentsSpeech.FirstTimeIntroduction)
-      .reprompt(CoreIntentsSpeech.FirstTimeIntroductionReprompt)
-      .getResponse();
+    return handlerInput.attributesManager.getPersistentAttributes().then(attributes => {
+      const favouriteTeam = get(attributes, ["favouriteTeam"]);
+      let usedOnce = get(attributes, ["usedOnce"], false);
+      let timesWithoutFavourite = get(attributes, ["timesWithoutFavourite"], 0);
+      if (usedOnce) {
+        if (favouriteTeam || (timesWithoutFavourite > 0 && timesWithoutFavourite < 5)) {
+          const newAttributes = {
+            ...attributes,
+            timesWithoutFavourite: favouriteTeam ? 1 : timesWithoutFavourite + 1
+          };
+          handlerInput.attributesManager.setPersistentAttributes(newAttributes);
+          return handlerInput.attributesManager
+            .savePersistentAttributes()
+            .then(() => {
+              return handlerInput.responseBuilder
+                .speak(CoreIntentsSpeech.Introduction)
+                .reprompt(CoreIntentsSpeech.IntroductionFallback)
+                .getResponse();
+            })
+            .catch(error => {
+              console.error(error);
+              return handlerInput.responseBuilder
+                .speak(ErrorsSpeech.GenericError)
+                .withShouldEndSession(true)
+                .getResponse();
+            });
+        }
+
+        const newAttributes = {
+          ...attributes,
+          timesWithoutFavourite: 1
+        };
+        handlerInput.attributesManager.setPersistentAttributes(newAttributes);
+
+        return handlerInput.attributesManager
+          .savePersistentAttributes()
+          .then(() => {
+            return handlerInput.responseBuilder
+              .speak(CoreIntentsSpeech.FavouriteTeamNotSetIntroduction)
+              .reprompt(CoreIntentsSpeech.IntroductionFallback)
+              .getResponse();
+          })
+          .catch(error => {
+            console.error(error);
+            return handlerInput.responseBuilder
+              .speak(ErrorsSpeech.GenericError)
+              .withShouldEndSession(true)
+              .getResponse();
+          });
+      }
+      const newAttributes = {
+        ...attributes,
+        usedOnce: true
+      };
+      handlerInput.attributesManager.setPersistentAttributes(newAttributes);
+      return handlerInput.attributesManager
+        .savePersistentAttributes()
+        .then(() => {
+          return handlerInput.responseBuilder
+            .speak(CoreIntentsSpeech.FirstTimeIntroduction)
+            .reprompt(CoreIntentsSpeech.FirstTimeIntroductionReprompt)
+            .getResponse();
+        })
+        .catch(error => {
+          console.error(error);
+          return handlerInput.responseBuilder
+            .speak(ErrorsSpeech.GenericError)
+            .withShouldEndSession(true)
+            .getResponse();
+        });
+    });
   }
 };
 
--- lambda/custom/resources/coreIntents.js
+++ lambda/custom/resources/coreIntents.js
@@ -9,15 +9,26 @@ const CoreIntentsSpeech = {
   FirstTimeIntroductionReprompt: `
     If you need help. Just say "help". To get started just say, "Next Fixture", or. "Last Result".
     `,
+  Introduction: `
+    Hi. Welcome back to the Rugby Guru. What can I help with today?
+  `,
+  IntroductionFallback: `
+    If you are unsure with what to do, just say "help". What can I help you with?
+  `,
+  FavouriteTeamNotSetIntroduction: `
+    Hi. Welcome back to the Rugby Guru. I've noticed that you haven't set a favourite team yet. To do so, just say "Set my favourite team".
+
+    What can I help you with today?
+  `,
   Help: `
     At the moment I can give you an update on the latest result and next fixture for any team in the Gallagher Premiership. Just say "Give me the next fixture" or "What was the last fixture".
-    `,
+  `,
   Goodbye: `
     Goodbye!
-    `,
+  `,
   Fallback: `
     Sorry, I didn't understand that. Why don't you try saying it again?
-    `
+  `
 };
 
 module.exports = {
